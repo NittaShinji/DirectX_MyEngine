11c4a7a08226a35d3d0d23f00e4587d76e56f013
Nittshinji <94505316+NittaShinji@users.noreply.github.com>
2022-06-29 15:37:42 +0900

    05_06 終了

--- BasicVS.hlsl
+++ BasicVS.hlsl
@@ -1,10 +1,14 @@
 #include"Basic.hlsli"
 
+//P04_01_P25
 VSOutput main(float4 pos : POSITION, float2 uv : TEXCOORD)
 {
 	VSOutput output; //ピクセルシェーダーに渡す値
 	//output.svpos = pos;
-	output.svpos = mul(mat, pos);//座標に行列を乗算
+	 
+	//システム用頂点座標
+	output.svpos = mul(mat, pos);//座標に3D変換行列を乗算
+	//UV値
 	output.uv = uv;
 	return output;
 }
