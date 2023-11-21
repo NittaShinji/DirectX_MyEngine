b5575421bd6adbcc5b64b1b50895280a1112765a
Nittshinji <94505316+NittaShinji@users.noreply.github.com>
2023-11-21 15:17:37 +0900

    カメラの必要ないコメントアウトを削除

--- Source/application/camera/GameCamera.cpp
+++ Source/application/camera/GameCamera.cpp
@@ -32,73 +32,15 @@ void GameCamera::Update(bool isPlayerMoving, Vector3 playerPos, Vector3 playerIn
 	if(isPlayerMoving == true)
 	{
 		goalEyePos_.x = playerPos.x - playerInitPos.x + initcameraEye.x + initEyeDistance_.x;
-		//goalEyePos_.y = (playerDeadPos.y - playerInitPos.y) / 1.6f + initcameraEye.y;
 		goalEyePos_.y = playerDeadPos.y - playerInitPos.y + initcameraEye.y;
-		//goalEyePos_.y = playerPos.y - playerInitPos.y + initcameraEye.y;
-
-
 		goalEyePos_.z = playerPos.z - playerInitPos.z + initcameraEye.z + initEyeDistance_.z;
 
 		goalEyeTarget_.x = playerPos.x - playerInitPos.x + initcameraTarget.x + initTargetDistance_.x;
-		//goalEyeTarget_.y = (playerDeadPos.y - playerInitPos.y) / 1.6f + initcameraTarget.y;
 		goalEyeTarget_.y = playerDeadPos.y - playerInitPos.y + initcameraTarget.y;
-		//goalEyeTarget_.y = playerPos.y - playerInitPos.y + initcameraTarget.y;
-
 		goalEyeTarget_.z = playerPos.z - playerInitPos.z + initcameraTarget.z + initTargetDistance_.z;
 
 		moveEyeVec = goalEyePos_ - eye_;
 		moveTargetVec = goalEyeTarget_ - target_;
-
-		/*if(isDead == true)
-		{
-			if(isStopTarget_ == false)
-			{
-				moveTargetVec = goalEyeTarget_ - target_;
-			}
-			
-			if(moveTargetVec.y >= 0.0f)
-			{
-				moveTargetVec.x = 0.0f;
-				moveTargetVec.y = 0.0f;
-				moveTargetVec.z = 0.0f;
-
-				isStopTarget_ = true;
-			}
-		}
-		else
-		{
-			moveTargetVec = goalEyeTarget_ - target_;
-
-		}
-		
-		if(isDead == true)
-		{
-			if(moveEyeVec.y > 0)
-			{
-				moveEyeVecY = moveEyeVec;
-				moveEyeVecY.Normalize();
-				moveEyeVec.z = 0.0f;
-			}
-			else
-			{
-				
-			}
-
-			if(moveTargetVec.y >= 0)
-			{
-				moveTargetVec.y = 0.0f;
-				moveTargetVecY.y = 0.0f;
-			}
-			else
-			{
-				moveTargetVecY = moveTargetVec;
-				moveTargetVecY.Normalize();
-			}
-		}
-		else
-		{
-			
-		}*/
 
 		moveEyeVecY = moveEyeVec;
 		moveEyeVecY.Normalize();
@@ -115,56 +57,11 @@ void GameCamera::Update(bool isPlayerMoving, Vector3 playerPos, Vector3 playerIn
 
 		if(isDead == false)
 		{
-			/*if(moveEyeVecY.y < 0.1f)
-			{
-				speedEasing_.time++;
-				if(speedEasing_.time > 0 && speedEasing_.time <= speedEasing_.totalTime)
-				{
-					cameraSpeedY_ = PlayEaseOutQuint(speedEasing_);
-				}
-				else
-				{
-					speedEasing_.time = 0.0f;
-					cameraSpeedY_ = 1.0f;
-					isFinishEasing_ = true;
-				}
-
-				if(moveEyeVec.y < 0.01)
-				{
-					isFinishEasing_ = true;
-				}
-			}
-			else
-			{
-				speedEasing_.time = 0.0f;
-			}*/
-
-			//if(moveEyeVecY.y != 0)
-			//{
-			//	if(slowTimer_ <= 0)
-			//	{
-			//		eye_.y += moveEyeVecY.y / initEyeDistance_.y * cameraSpeedY_;
-			//		Vector3 moveTargetVecY = moveTargetVec;
-			//		target_.y += moveTargetVecY.y / initTargetDistance_.y * cameraSpeedY_;
-			//		slowTimer_ = kSlowTime_;
-			//	}
-			//	else
-			//	{
-			//		slowTimer_--;
-			//	}
-			//}
-
-			
 			eye_.y += moveEyeVecY.y / (distanceY + EyeYAxelRate_) * cameraSpeedY_;
 			target_.y += moveTargetVecY.y / (distanceY + EyeYAxelRate_) * cameraSpeedY_;
-			
-
 		}
 
 		if(playerAxel.x < 0) {}
-
-		//eye_.y += moveEyeVecY.y / (distanceY + EyeYAxelRate_) * cameraSpeedY_;
-		//target_.y += moveTargetVecY.y / (distanceY + EyeYAxelRate_) * cameraSpeedY_;
 
 		target_.x += moveTargetVec.x * cameraSpeed_;
 		target_.z += moveTargetVec.z / initTargetDistance_.z * cameraSpeed_;
@@ -177,41 +74,6 @@ void GameCamera::Update(bool isPlayerMoving, Vector3 playerPos, Vector3 playerIn
 				target_.y += moveTargetVecY.y / (distanceY + EyeYAxelRate_) * cameraSpeedY_;
 
 			}
-
-			//落下時視点
-			if(moveEyeVec.y < 0)
-			{
-				//eye_.y += moveEyeVecY.y / (distanceY + initEyeDistance_.y) * cameraSpeedY_;
-			}
-			
-			//落下時ターゲット
-			if(moveTargetVec.y < 0)
-			{
-				//target_.y += moveTargetVecY.y / (distanceY + initEyeDistance_.y) * cameraSpeedY_;
-			}
-			
-			//eye_.y += moveEyeVecY.y / (distanceY + initEyeDistance_.y) * cameraSpeedY_;
-			//target_.y += moveTargetVecY.y / (distanceY + initEyeDistance_.y) * cameraSpeedY_;
-
-			/*if(moveEyeVecY.y < 0)
-			{
-				speedEasing_.time++;
-				if(speedEasing_.time > 0 && speedEasing_.time <= speedEasing_.totalTime)
-				{
-					isFinishEasing_ = true;
-					cameraSpeedY_ = PlayEaseOutQuint(speedEasing_);
-				}
-				else
-				{
-					speedEasing_.time = 0.0f;
-					cameraSpeedY_ = 1.0f;
-					isFinishEasing_ = true;
-				}
-			}
-			else
-			{
-				speedEasing_.time = 0.0f;
-			}*/
 		}
 	}
 
@@ -260,7 +122,6 @@ void GameCamera::ImGuiUpdate()
 
 void GameCamera::GoalAnimation()
 {
-
 	if(sceneAnimeTimer_ >= 0)
 	{
 		sceneAnimeTimer_--;
